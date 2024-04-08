e61e2170ee392f566c08e533932201703463f018
rubiigen <alyxwren@proton.me>
2024-04-08 04:30:16 +0100

    finalisation

---
+++ default/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Your new nix config";
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      #"aarch64-linux"
+      "x86_64-linux"
+      #"i686-linux"
+      #"aarch64-darwin"
+      #"x86_64-darwin"
+    ];
+  in {
+    # Entrypoint for NixOS configurations
+    nixosConfigurations = import ./hosts {inherit self;};
+
+    # devshells that are provided by this flake
+    # adding more packages to buildInputs makes them available
+    # while inside the devshell - enetered via `nix develop`
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra # opionated Nix formatter
+
+            # example of bootstrapping self-contained shell
+            # applications for your flake
+            # this adds an `update` command to your shell
+            # which'll update all inputs and commit
+            (writeShellApplication {
+              name = "update";
+              text = ''
+                nix flake update && git commit flake.lock -m "flake: bump inputs"
+              '';
+            })
+          ];
+        };
+      }
+    );
+
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
+
+  inputs = {
+    # Nixpkgs (unstable)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # nixos-hardware
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+}
--- default/homes/username/default.nix
+++ default/homes/username/default.nix
@@ -5,9 +5,12 @@
   ...
 }: {
   imports = [
-    ./packages.nix # home.packages and similar stuff
-    ./programs.nix # programs.<programName>.enable
-    ./git.nix
+    ../../common/arrpc.nix
+    ../../common/packages.nix # home.packages and similar stuff
+    ../../common/programs.nix # programs.<programName>.enable
+    ../../common/gita.nix
+    ../../common/nvim-flake.nix
+    ../../common/ags
   ];
 
   home = {
